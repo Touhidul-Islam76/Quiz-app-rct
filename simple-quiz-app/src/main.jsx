1845c70195a7710d727097ab821bf6a135e0120f
Touhidul Islam <mdtouhidul426@gmail.com>
2026-09-05 02:17:19 +0600

    Update main.jsx

--- simple-quiz-app/src/main.jsx
+++ simple-quiz-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import App from './App.jsx'
+
+import { router } from './router/index.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
